3c2c72d80ebf46007f9fcd3ef9b502496b10ef6d
luismoramedina <luismoramedina@gmail.com>
2018-02-22 12:27:09 +0100

    logs plain sec context

--- samples/stars/src/main/groovy/com/github/luismoramedina/stars/StarsApplication.groovy
+++ samples/stars/src/main/groovy/com/github/luismoramedina/stars/StarsApplication.groovy
@@ -1,23 +1,35 @@
 package com.github.luismoramedina.stars
 
+import groovy.util.logging.Slf4j
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @SpringBootApplication
+@Slf4j
 class StarsApplication {
 
 	static void main(String[] args) {
 		SpringApplication.run StarsApplication, args
 	}
 
-	@RequestMapping
-	Star star() {
+
+	@RequestMapping(value = "/stars/{id}", method=RequestMethod.GET)
+	Star star(
+			@PathVariable String id,
+			@RequestHeader(value="Authorization", required = false) String auth,
+			@RequestHeader(value="Plain-authorization", required = false) String plainAuth) {
+
+		log.info "plainAuth: " + plainAuth
+		log.info "auth: " + auth
 		def star = new Star()
 		star.number = 5
-		star.id = 1
+		star.id = id
 		star
 	}
 }
